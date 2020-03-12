ed9d8aad5374487732a196e76f97864907b79832
G-Rad <gradney@gmail.com>
2020-03-11 21:42:36 -0400

    Added functioning button on simple form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  
   appTitle = 'sonlit';
+
+  addStudyGuide(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding study guide titled: ${title.value} and linked: ${link.value}`);
+    return false;
+  }
 }
